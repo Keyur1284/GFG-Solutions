ef4cf1eb9b1547814c712d1b5463573baf643ade
Govrani Keyur <202101498@daiict.ac.in>
2023-05-19 02:59:59 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -7,35 +7,114 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
+class DSU {
+
+private:
+    
+    vector<int> parent, rank, size;
+    
 public:
-    
-    int dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, 1, -1};
-    
-    void DFS (int x, int y, int n, int m, vector<vector<bool>> &vis, vector<vector<char>> &mat)
+
+    DSU (int n)
     {
-        vis[x][y] = true;
-        
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i <= n; i++)
         {
-            int newx = x + dx[i];
-            int newy = y + dy[i];
-            
-            if (newx >= 0 && newx < n && newx >= 0 && newy < m && mat[newx][newy] == 'O' && !vis[newx][newy])
-                DFS (newx, newy, n, m, vis, mat);
+            parent.emplace_back(i);
+            rank.emplace_back(0);
+            size.emplace_back(1);
         }
     }
 
+    int findPar (int node)
+    {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findPar(parent[node]);
+    }
+
+    void UnionRank (int u, int v)
+    {
+        u = findPar(u);
+        v = findPar(v);
+
+        if (u == v)
+            return;
+
+        if (rank[u] < rank[v])
+        {
+            parent[u] = v;
+        }
+
+        else if (rank[u] > rank[v])
+        {
+            parent[v] = u;
+        }
+
+        else if (rank[u] == rank[v])
+        {
+            parent[v] = u;
+            rank[u]++;
+        }
+    }
+
+    void UnionSize (int u, int v)
+    {
+        u = findPar(u);
+        v = findPar(v);
+
+        if (u == v)
+            return;
+
+        if (size[u] < size[v])
+        {
+            parent[u] = v;
+            size[v] += size[u];
+        }
+
+        else if (rank[u] >= rank[v])
+        {
+            parent[v] = u;
+            size[u] += size[v];
+        }
+    }    
+};
+
+
+class Solution{
+public:
+
+    int dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
+
     vector<vector<char>> fill(int n, int m, vector<vector<char>> &mat)
     {
-        vector<vector<bool>> vis(n, vector<bool> (m, false));
+        DSU dsu(n * m);
+        int boundary = m * n;
         
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
-                if ((i * j == 0 || i == n - 1 || j == m - 1) && mat[i][j] == 'O' && !vis[i][j])
-                    DFS (i, j, n, m, vis, mat);
+                if ((i * j == 0 || i == n - 1 || j == m - 1) && mat[i][j] == 'O')
+                    dsu.UnionRank (i * m + j, boundary);
+            }
+        }
+        
+        for (int i = 1; i < n - 1; i++)
+        {
+            for (int j = 1; j < m - 1; j++)
+            {
+                if (mat[i][j] == 'O')
+                {
+                    for (int dir = 0; dir < 4; dir++)
+                    {
+                        int newx = i + dx[dir];
+                        int newy = j + dy[dir];
+
+                        if (newx >= 0 && newx < n && newy >= 0 && newy < m && mat[newx][newy] == 'O')
+                            dsu.UnionRank(i * m + j, newx * m + newy);
+                    }
+                }
             }
         }
         
@@ -43,7 +122,7 @@ public:
         {
             for (int j = 0; j < m; j++)
             {
-                if (mat[i][j] == 'O' && !vis[i][j])
+                if (mat[i][j] == 'O' && dsu.findPar(i * m + j) != dsu.findPar(boundary))
                     mat[i][j] = 'X';
             }
         }
