709deed4cdde888e4cdc04806bf3dcb73659fad4
Govrani Keyur <202101498@daiict.ac.in>
2023-05-23 20:24:12 +0530

    Added solution - LeetHub

--- Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
+++ Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
@@ -6,32 +6,43 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    
-    void expand (int left, int right, int &n, string &ans, string &s)
-    {
-        while (left >= 1 && right <= n && s[left] == s[right])
-        {
-            left--;
-            right++;
-        }
-
-        int len = right - left - 1;
-
-        if (ans.size() < len)
-            ans = s.substr(left + 1, len); 
-    }
   
     string longestPalin (string s) {
         
         int n = s.size();
-        s = '!' + s;
+        string st = string(2 * n + 1, '!');
+
+        for (int i = 0; i < n; i++)
+        {
+            st[2 * i + 1] = s[i];
+        }
+
+        vector<int> pal(2 * n + 1, 0);
+        int longestCenter = 0, longestLength = 0;
+        
+        for (int i = 0; i < 2 * n + 1; i++)
+        {
+            int l = i - pal[i] - 1;
+            int r = i + pal[i] + 1;
+            
+            while (l >= 0 && r < 2 * n + 1 && st[l] == st[r])
+            {
+                l--;
+                r++;
+                pal[i]++;
+            }
+            
+            if (pal[i] > longestLength)
+            {
+                longestLength = pal[i];
+                longestCenter = i;
+            }
+        }
+        
         string ans = "";
 
-        for (int i = 1; i <= n; i++)
-        {
-            expand(i, i, n, ans, s);
-            expand(i, i + 1, n, ans, s);
-        }
+        for (int index = longestCenter - longestLength + 1; index < longestCenter + longestLength; index += 2)
+            ans += st[index];
 
         return ans;
     }
