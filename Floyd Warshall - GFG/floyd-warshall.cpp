be3c0ef47c63c9fdf4e15fb144b123478b368c7a
Govrani Keyur <202101498@daiict.ac.in>
2023-05-25 03:04:42 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -11,33 +11,51 @@ class Solution {
   public:
 	void shortest_distance(vector<vector<int>>&matrix){
 	    
-	    int n = matrix.size();
-	    
+	    int n = matrix.size(), inf = INT_MAX;
+
 	    for (int i = 0; i < n; i++)
 	    {
 	        for (int j = 0; j < n; j++)
 	        {
 	            if (matrix[i][j] == -1)
-	                matrix[i][j] = 1e9;
+	                matrix[i][j] = inf;
 	        }
 	    }
-	    
+
 	    for (int via = 0; via < n; via++)
 	    {
 	        for (int i = 0; i < n; i++)
     	    {
     	        for (int j = 0; j < n; j++)
     	        {
-    	            matrix[i][j] = min(matrix[i][j], matrix[i][via] + matrix[via][j]);
+					if (matrix[i][via] < inf && matrix[via][j] < inf && matrix[i][via] + matrix[via][j] < inf)
+    	            	matrix[i][j] = min(matrix[i][j], matrix[i][via] + matrix[via][j]);
     	        }
     	    }
 	    }
-	    
+
+        // For detection of negative cycle, we have to write following code
+
+        /*
+            bool checkNegativeCycle (vector<vector<int>>&matrix)
+            {
+                int n = matrix.size();
+
+                for (int node = 0; node < n; node++)
+                {
+                    if (matrix[node][node] < 0)
+                        return true;
+                }
+
+                return false
+            }
+        */
+
 	    for (int i = 0; i < n; i++)
 	    {
 	        for (int j = 0; j < n; j++)
 	        {
-	            if (matrix[i][j] == 1e9)
+	            if (matrix[i][j] == inf)
 	                matrix[i][j] = -1;
 	        }
 	    }
