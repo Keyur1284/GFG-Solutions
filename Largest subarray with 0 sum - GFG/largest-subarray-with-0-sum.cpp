b8cb796695bbe2d718d3063b8493ba20b79a9a20
Govrani Keyur <202101498@daiict.ac.in>
2023-06-08 19:34:24 +0530

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
@@ -15,13 +15,11 @@ class Solution{
         unordered_map<int, int> mp;
         int sum = 0;
         int maxi = 0;
+        mp[0] = -1;
         
         for (int i = 0; i < n; i++)
         {
             sum += arr[i];
-            
-            if (sum == 0)
-                maxi = i + 1;
             
             if (mp.find(sum) == mp.end())
                 mp[sum] = i;
