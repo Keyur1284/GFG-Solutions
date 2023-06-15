2e3a9d97034c1e9234fb8e3c0910a5478c3b5845
Govrani Keyur <202101498@daiict.ac.in>
2023-06-15 12:49:54 +0530

    Added solution - LeetHub

--- Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
+++ Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
@@ -9,33 +9,35 @@ class Solution {
     string longestPalin (string s) {
         
         int n = s.size();
-        string st = string(2 * n + 1, '!');
-
+        string st = string (2 * n + 1, '!');
+        
         for (int i = 0; i < n; i++)
-        {
             st[2 * i + 1] = s[i];
-        }
-
-        vector<int> pal(2 * n + 1, 0);
-        int longestLength = 0, longestCenter = 0, center = 0, right = 0;
+            
+        int center = 0, right = 0, longestCenter = 0, longestLength = 0;
+        vector<int> pal (2 * n + 1, 0);
         
         for (int i = 0; i < 2 * n + 1; i++)
         {
             int mirror = 2 * center - i;
             
             if (right - i > 0)
-            {
                 pal[i] = min(right - i, pal[mirror]);
-            }
-            
+                
             int l = i - pal[i] - 1;
             int r = i + pal[i] + 1;
             
             while (l >= 0 && r < 2 * n + 1 && st[l] == st[r])
             {
+                pal[i]++;
                 l--;
                 r++;
-                pal[i]++;
+            }
+            
+            if (i + pal[i] > right)
+            {
+                center = i;
+                right = i + pal[i];
             }
             
             if (pal[i] > longestLength)
@@ -43,18 +45,12 @@ class Solution {
                 longestLength = pal[i];
                 longestCenter = i;
             }
-            
-            if (i + pal[i] > right)
-            {
-                right = i + pal[i];
-                center = i;
-            }
         }
         
         string ans = "";
         
-        for (int index = longestCenter - longestLength + 1; index < longestCenter + longestLength; index += 2)
-            ans += st[index];
+        for (int i = longestCenter - longestLength + 1; i < longestCenter + longestLength; i+= 2)
+            ans += st[i];
             
         return ans;
     }
