935eaa68596e200e76fb8948609fe151f5b26e1f
Govrani Keyur <202101498@daiict.ac.in>
2023-07-18 01:01:26 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -6,18 +6,18 @@ using namespace std;
 class Solution {
 	public:
 	       
-	   int solve (int i, int j, int n, vector<vector<int>> &dp, string &str)
+	   int solve (int i, int j, vector<vector<int>> &dp, string &str)
 	   {
-	       if (i == n || j == n)
+	       if (i < 0 || j < 0)
 	           return 0;
 	           
 	       if (dp[i][j] != -1)
 	           return dp[i][j];
 	           
 	       if (str[i] == str[j] && i != j)
-	           return 1 + solve (i + 1, j + 1, n, dp, str);
+	           return 1 + solve (i - 1, j - 1, dp, str);
 	           
-	       return dp[i][j] = max(solve(i, j + 1, n, dp, str), solve(i + 1, j, n, dp, str));
+	       return dp[i][j] = max(solve(i, j - 1, dp, str), solve(i - 1, j, dp, str));
 	   }
 	    
        int LongestRepeatingSubsequence(string str){
@@ -25,7 +25,7 @@ class Solution {
 		    int n = str.length();
 		    vector<vector<int>> dp (n, vector<int> (n, -1));
 		    
-		    return solve (0, 0, n, dp, str);
+		    return solve (n - 1, n - 1, dp, str);
 		}
 
 };
