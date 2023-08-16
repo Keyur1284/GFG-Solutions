d6aa3238e0a2e3edc7d04cbddb8332d2e15f1e61
Govrani Keyur <202101498@daiict.ac.in>
2023-08-16 11:31:24 +0530

    Added solution - LeetHub

--- Nth catalan number - GFG/nth-catalan-number.cpp
+++ Nth catalan number - GFG/nth-catalan-number.cpp
@@ -7,44 +7,23 @@ class Solution
 {
     public:
     //Function to find the nth catalan number.
-    int MOD = 1e9 + 7;
-    
-    int modPow (int base, int exp)
-    {
-        int ans = 1;
-        
-        while (exp)
-        {
-            if (exp & 1)
-                ans = (1LL * ans * base) % MOD;
-                
-            exp >>= 1;
-            base = (1LL * base * base) % MOD;
-        }
-        
-        return ans;
-    }
-
-    int modInv (int num)
-    {
-        return modPow(num, MOD - 2); 
-    }
-    
     int findCatalan(int n) 
     {
-        vector<int> fact(2 * n + 1);
-        fact[0] = 1;
+        int MOD = 1e9 + 7;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
         
-        for (int i = 1; i <= 2 * n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            fact[i] = (1LL * fact[i - 1] * i) % MOD;
+            for (int j = 0; j < i; j++)
+            {
+                int ways = (1LL * dp[j] * dp[i - j - 1]) % MOD;
+                dp[i] += ways;
+                dp[i] %= MOD;
+            }
         }
         
-        int catalan = fact[2 * n];
-        catalan = (1LL * catalan * modInv(fact[n + 1])) % MOD;
-        catalan = (1LL * catalan * modInv(fact[n])) % MOD;
-        
-        return catalan;
+        return dp[n];
     }
 };
 
