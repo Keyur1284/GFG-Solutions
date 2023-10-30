75c37d5ebf69cd7a25db21b082f94f669afa5932
Govrani Keyur <202101498@daiict.ac.in>
2023-10-30 19:35:55 +0530

    Added solution - LeetHub

---
+++ Sum of XOR of all pairs - GFG/sum-of-xor-of-all-pairs.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// An efficient C++ program to compute 
+// sum of bitwise OR of all pairs
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    // Returns sum of bitwise OR
+    // of all pairs
+    long long int sumXOR(int arr[], int n)
+    {
+    	
+    	long long sum = 0;
+    	
+    	for (int pos = 0; pos < 32;  pos++)
+    	{
+    	    long long ones = 0;
+    	    
+    	    for (int i = 0; i < n; i++)
+    	    {
+    	        ones += (arr[i] & (1 << pos)) ? 1 : 0;
+    	    }
+    	    
+    	    sum += (ones * (n - ones) * (1 << pos));
+    	}
+    	
+    	return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	 int n ;
+	 cin>>n;
+	 int arr[n+1];
+	 for( int i=0;i<n;i++)
+	    cin>>arr[i];
+	 Solution ob;
+	 cout<<ob.sumXOR(arr, n)<<endl;
+	}	
+	return 0;
+}
+
+// } Driver Code Ends
